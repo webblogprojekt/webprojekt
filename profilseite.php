bb88daa0647e347e2163ac4085011f4ad7ea8357
af095 <af095@hdm-stuttgart.de>
2020-04-30 22:44:21 +0200

    Profilpage

---
+++ profilseite.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Erfolg | Your Profilpage</title>
+</head>
+<body>
+    <h1>Erfolg</h1>
+
+    <h2>Your Profilpage</h2>
+    <?php
+    $userid = $_GET['id'];
+
+    ?>
+    Username: <br>
+    Age: <br>
+    Subject: <br>
+    Biography: <br>
+</body>
+</html>
+
